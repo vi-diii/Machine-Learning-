a7633b9da2b0cb046643448f388bbe55c0ba7c2b
vi-diii <vynt20416c@st.uel.edu.vn>
2023-12-03 19:23:37 +0700

    Exercise

---
+++ THỰC HÀNH-20231203T121230Z-001/THỰC HÀNH/FileUtil.py
@@ -0,0 +1,19 @@
+import pickle
+class FileUtil:
+    @staticmethod
+    def savemodel(model, filename):
+        try:
+            pickle.dump(model,open(filename,'wb'))
+            return True
+        except:
+            print('An exception occurred')
+            return False
+
+    @staticmethod
+    def loadmodel(filename):
+        try:
+            model = pickle.load(open(filename, 'rb'))
+            return model
+        except:
+            print('An exception occurred')
+            return None
